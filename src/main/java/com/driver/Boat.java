63b8c556ef0f59fcd260bf196c34c44c1ef89331
vivek ramani <92964991+vramani1121@users.noreply.github.com>
2023-02-16 22:17:00 +0530

    ffghv

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,10 +4,10 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
-    public Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
+//    public Boat(String name, int capacity) {
+//        this.name = name;
+//        this.capacity = capacity;
+//    }
 
     @Override
     public String getVehicleName() {
